dc8b21cfdb1a97a2ce6a8acbefaec07fe7f14f50
Pooja4183 <poojatomar.jt@gmail.com>
2023-05-24 15:52:59 +0530

    Draft product listing buy page

--- src/component/sell/ContactForm.js
+++ src/component/sell/ContactForm.js
@@ -1,6 +1,6 @@
-import * as React from 'react';
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
+import * as React from "react";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
 import Sellstyle from "./WhoAreYouForm.module.css";
 import StyledButton from "../custom/StyledButton";
@@ -16,59 +16,46 @@ const ContactForm = ({
   contactInfo,
   handleContactChange,
 }) => {
-  
   function handleSubmit(event) {
     event.preventDefault();
   }
 
   return (
-   <>
+    <>
+      <Grid container className={Sellstyle.formstyleform}>
+        <NestedRightGrid
+          title={"Is This The Correct Address? If Not Click Here?"}
+          value={houseWorthInfo.address}
+        />
+        <NestedLeftGrid title={"Who Are You?"}>
+          <form onSubmit={handleSubmit}>
+            <Box
+              component="form"
+              sx={{
+                "& > :not(style)": { m: 1, width: "50%" },
+              }}
+              noValidate
+              autoComplete="off"
+            >
+              <TextField
+                id="outlined-basic"
+                label="Your Full Name"
+                variant="outlined"
+                sx={{ marginTop: "0px" }}
+              />
+              <TextField id="outlined-basic" label="Email" variant="outlined" />
+              <TextField id="outlined-basic" label="Phone" variant="outlined" />
+            </Box>
 
-<Grid container className={Sellstyle.formstyleform}>
-      <NestedRightGrid
-        title={"Is This The Correct Address? If Not Click Here?"}
-        value={houseWorthInfo.address}
-      />
-      <NestedLeftGrid title={"Who Are You?"}>
-        <form onSubmit={handleSubmit}>
+            <StyledButton type="submit" variant="outlined">
+              Next
+            </StyledButton>
+          </form>
+        </NestedLeftGrid>
+      </Grid>
 
-        <Box
-    component="form"
-    sx={{
-      '& > :not(style)': { m: 1, width: '50%' },
-    }}
-    noValidate
-    autoComplete="off"
-  >
-    <TextField id="outlined-basic" label="Your Full Name" variant="outlined" sx={{marginTop:'0px'}} />
-    <TextField id="outlined-basic" label="Email" variant="outlined" />
-    <TextField id="outlined-basic" label="Phone" variant="outlined" />
-   
-  </Box>
-           
-
-              <StyledButton type="submit" variant="outlined">
-                Next
-              </StyledButton>
-            
-        
-        </form>
-      </NestedLeftGrid>
-    </Grid>
-
-
-
-
-
-{/*  */}
-
-
-
-
-
-
-   
-  </>
+      {/*  */}
+    </>
   );
 };
 
---
+++ src/pages/Buy.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Footer from '../component/Footer';
+import Header from '../component/Header';
+import BuyProducts from '../component/buy/BuyProducts';
+
+
+const Buy = () => {
+  return (
+    <>
+    <Header /> 
+      <BuyProducts title="DLF Phase 1"/>
+      <Footer />
+    </>
+  );
+};
+
+export default Buy;
