16d96f683285aeb7d5cae8852fc5da782234b3b4
Pooja4183 <poojatomar.jt@gmail.com>
2023-05-27 19:40:06 +0530

    Removed SellAnyHome Typography

--- src/component/Header.js
+++ src/component/Header.js
@@ -86,25 +86,6 @@ function ResponsiveAppBar() {
               ))}
             </Menu>
           </Box>
-          {/* <AdbIcon sx={{ display: { xs: 'flex', md: 'none' }, mr: 1 }} /> */}
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href=""
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            SellAnyHome
-          </Typography>
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } ,justifyContent: 'flex-end', mr:2 }}>
             {pages.map((page) => (
               <Button
