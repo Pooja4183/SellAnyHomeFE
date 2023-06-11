d9aafd5f48370db7a6031a12012f255187a46d2e
Pooja4183 <poojatomar.jt@gmail.com>
2023-06-11 22:27:23 +0530

    Added draft Property Form

--- src/component/admin/AdminPanel.js
+++ src/component/admin/AdminPanel.js
@@ -4,6 +4,7 @@ import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import PropertyGrid from "./PropertyGrid";
+import PropertyForm from "./PropertyForm";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -28,7 +29,9 @@ const AdminPanel = () => {
           </Item>
         </Grid>
         <Grid item xs={6}>
-          <Item>2</Item>
+          <Item>
+            <PropertyForm/>
+          </Item>
         </Grid>
       </Grid>
     </Box>
