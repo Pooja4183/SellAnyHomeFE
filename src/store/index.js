81043fe76296c9847dba639a145158272c4c69b4
Pooja4183 <poojatomar.jt@gmail.com>
2023-06-26 14:14:07 +0530

    Fixed page refresh issue

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import productSlice from './productSlice';
 import cartSlice from './cartSlice';
 import buyContactSlice from './buyContactSlice';
 import adminSlice from './adminSlice';
 
-export const store = configureStore({
-  reducer: {
-    products: productSlice.reducer,
-    cart: cartSlice.reducer,
-    buyContact: buyContactSlice.reducer,
-    admin: adminSlice.reducer
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const rootReducer = combineReducers({
+  products: productSlice.reducer,
+  cart: cartSlice.reducer,
+  buyContact: buyContactSlice.reducer,
+  admin: adminSlice.reducer
 });
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+export const persistor = persistStore(store);
