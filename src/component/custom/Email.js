83e1ee35c92562cccba03e0d9f36a42b64c01073
Pooja4183 <poojatomar.jt@gmail.com>
2023-10-05 14:32:14 +0530

    Added Email, Phone and Whatsapp calling features

---
+++ src/component/custom/Email.js
@@ -0,0 +1,16 @@
+import EmailIcon from '@mui/icons-material/Email';
+
+const Email = ({ objectWithEmail}) => {
+    console.log("Number: ", objectWithEmail);
+    let numberToDisplay = "contact@tomorrow.com";
+    if(objectWithEmail) {
+        numberToDisplay =  objectWithEmail.email;
+    }
+  return (
+    <a href={`tel:${numberToDisplay}`} style={{ textDecoration: 'none', color: 'inherit' }}>
+      <EmailIcon style={{ verticalAlign: 'middle', color:'blueviolet' }}  /> {numberToDisplay}
+    </a>
+  );
+};
+
+export default Email;
