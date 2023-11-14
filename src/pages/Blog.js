e606c079a93b190f0a51d2b507e1028c4366b9cd
Pooja4183 <poojatomar.jt@gmail.com>
2023-11-14 23:23:50 +0530

    Made changes for BlogDetail and renamed Blog to ListBlogs

--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -3,12 +3,14 @@ import Header from '../component/Header';
 import Footer from '../component/Footer';
 import ExclusiveProducts from '../component/product/ExclusiveProducts'
 import BlogDetail from '../component/blog/BlogDetail';
+import ListBlogs from '../component/blog/ListBlogs';
 
 const Blog = () => {
   return (
     <>
       <Header showSearch={true}/>
       <BlogDetail />
+      <ListBlogs title={"You might be interested in these articles!"}/>
       <ExclusiveProducts title={"Similar Properties"}/>
       <Footer />
     </>
