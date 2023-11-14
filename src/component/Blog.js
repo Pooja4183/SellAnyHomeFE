ac44a878d56dbf94cdbca85f273cbd1d5fa3d7ab
Pooja4183 <poojatomar.jt@gmail.com>
2023-11-14 15:23:29 +0530

    Show Blog on home page

--- src/component/Blog.js
+++ src/component/Blog.js
@@ -1,13 +1,11 @@
-import * as React from 'react';
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
 import { experimentalStyled as styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import { Box, Card, CardActionArea, CardContent, CardMedia, Grid, Paper, Typography } from '@mui/material';
+import { fetchBlogs } from '../store/adminAction';
+import { convertFromRaw, stateToHTML } from 'draft-js-export-html';
+
 import bolgImg from '../images/news.jpg';
 import bImg from '../images/bImg.jpg';
 import blImg from '../images/blImg.jpg';
@@ -23,12 +21,55 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 export default function ResponsiveGrid() {
+  const dispatch = useDispatch();
+  const blogs = useSelector((state) => state.admin.blogs);
+
+  useEffect(() => {
+    dispatch(fetchBlogs());
+  }, [dispatch]);
+
+  const renderContent = (contentState) => {
+    if (!contentState || !contentState.blocks) {
+      return null;
+    }
+
+    const entityMap = contentState.entityMap || {};
+
+    const textBlocks = [];
+    let imageBlock = bolgImg;
+
+    contentState.blocks.forEach((block, index) => {
+      switch (block.type) {
+        case 'unstyled':
+          textBlocks.push(<div key={index} dangerouslySetInnerHTML={{ __html: block.text }} />);
+          break;
+        case 'atomic':
+          if (block.entityRanges.length > 0) {
+            const entityKey = block.entityRanges[0].key;
+            const entity = entityMap[entityKey];
+            if (entity.type === 'IMAGE') {
+              imageBlock = entity.data.url;
+            }
+          }
+          break;
+        default:
+          break;
+      }
+    });
+
+    return { textBlocks, imageBlock };
+  };
+
   return (
-    <Box sx={{ flexGrow: 1 }}>
-
-      <Grid container spacing={{ xs: 2, md: 1}} columns={{ xs: 4, sm: 8, md: 12 }} className={styles.newsContainer} >
-
-      <Grid container spacing={2} className={styles.newsHeading} >
+    blogs && (
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid
+          container
+          spacing={{ xs: 2, md: 1 }}
+          columns={{ xs: 4, sm: 8, md: 12 }}
+          className={styles.newsContainer}
+        >
+             <Grid container spacing={2} className={styles.newsHeading} >
             <Grid item xs={12}>
               <Item sx={{ borderRadius: 0, boxShadow: "none" , textAlign:'center', padding:0 ,}}>
                 <Typography variant='h2'>Latest Updates and News</Typography>
@@ -36,110 +77,35 @@ export default function ResponsiveGrid() {
               
             </Grid>
           </Grid>
-
-       
-          <Grid item xs={12} sm={6} md={4} sx={{padding: 0}} >
-
-
-            <Item  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' ,boxShadow:'none'}}>
-            <Card sx={{ maxWidth: 345 ,boxShadow:'none'}} className={styles.cardImg}>
-      <CardActionArea >
-        <CardMedia
-          component="img"
-          height="200"
-          image={bolgImg}
-          alt="green iguana"
-          
-        />
-        <CardContent className={styles.newsCont}>
-          <Typography gutterBottom component="div" className={styles.newsContH}>
-          Rent or buy? Here’s how to make that decision?
-          </Typography>
-          <Typography variant="body2" color="text.secondary" >
-          Discover some of the very best apartments, penthouses, townhouses and villas for rent across Dubai. 
-          Located in some of the most prime communities, these homes are designed to cater to every type of lifestyle.
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
-    </Item>
-
-          </Grid>
-          <Grid item xs={12} sm={6} md={4} sx={{padding: 0}} >
-
-
-<Item  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' ,boxShadow:'none'}}>
-<Card sx={{ maxWidth: 345 ,boxShadow:'none'}} className={styles.cardImg}>
-<CardActionArea >
-<CardMedia
-component="img"
-height="200"
-image={blImg}
-alt="green iguana"
-
-/>
-<CardContent className={styles.newsCont}>
-<Typography gutterBottom component="div" className={styles.newsContH}>
-A home where every view is a view to live for.
-</Typography>
-<Typography variant="body2" color="text.secondary" >
-What truly sets Golf Place Terraces apart are views of the clean-cut golf course, lush fairways, 
-winding walkways, landscaped parks and gardens, as well as vast open spaces that would enhance the life of every resident.
-</Typography>
-</CardContent>
-</CardActionArea>
-</Card>
-</Item>
-
-</Grid>
-<Grid item xs={12} sm={6} md={4} sx={{padding: 0}} >
-
-
-<Item  sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' ,boxShadow:'none'}}>
-<Card sx={{ maxWidth: 345 ,boxShadow:'none'}} className={styles.cardImg}>
-<CardActionArea >
-<CardMedia
-component="img"
-height="200"
-image={bImg}
-alt="green iguana"
-
-/>
-<CardContent className={styles.newsCont}>
-<Typography gutterBottom component="div" className={styles.newsContH}>
-Your Trusted Property Finder In Dubai
-</Typography>
-<Typography variant="body2" color="text.secondary" >
-Dubai’s latest and unprecedented residential marina masterpiece on the Arabian Gulf coast is driven by a desire to change the meaning of sophistication.
- An architectural endeavor that offers luxurious seafront living.
-</Typography>
-</CardContent>
-</CardActionArea>
-</Card>
-</Item>
-
-</Grid>
-
-    
-      </Grid>
-    </Box>
+          {blogs.map((blog) => (
+            <Grid item xs={12} sm={6} md={4} sx={{ padding: 0 }} key={blog._id}>
+              <Item
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  boxShadow: 'none',
+                  maxWidth: 345,
+                }}
+              >
+                <Card sx={{ boxShadow: 'none' }}>
+                  <CardActionArea>
+                    <CardMedia component="img" height="200" image={renderContent(blog.content).imageBlock} alt="green iguana" />
+                  </CardActionArea>
+                  <CardContent className={styles.newsCont}>
+                    <Typography gutterBottom component="div" className={styles.newsContH}>
+                      {blog.title}
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                      {blog.content ? renderContent(blog.content).textBlocks : 'No content available'}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              </Item>
+            </Grid>
+          ))}
+        </Grid>
+      </Box>
+    )
   );
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
