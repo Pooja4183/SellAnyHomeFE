f09c20c3f49a13c83d2eb9ff70dce7e60c576007
Pooja4183 <poojatomar.jt@gmail.com>
2023-11-14 21:54:55 +0530

    cosmetic changes

--- src/component/blog/Blog.js
+++ src/component/blog/Blog.js
@@ -21,7 +21,6 @@ import styles from "./blog.module.css";
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
-  //  padding: theme.spacing(2),
   textAlign: "left",
   color: theme.palette.text.secondary,
   boxShadow: "none",
